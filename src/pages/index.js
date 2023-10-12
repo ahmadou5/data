119b81c8f95496943ed9a010bd8425eb4f4f2d3b
ahmadou5 <ahmadlasauwal@gmail.com>
2023-10-12 03:11:44 +0300

    'first-commit'

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,14 @@ import { Inter, Lateef, Poppins } from "next/font/google";
 import { FiArrowRight } from "react-icons/fi";
 import Link from "next/link";
 
-const lateef = Lateef({ subsets: ["latin"], weight: "500" });
-const poppins = Poppins({ subsets: ["latin"], weight: "200" });
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ['latin'] , weight: '200' })
 
 export default function Home() {
   return (
     <main
-      className={`flex b min-h-screen flex-col items-center justify-between p-24 ${poppins.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-14 ${poppins.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-2xl lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-2xl lg:flex">
         <div className="text-center">
           <p className=" text-2xl font-semibold ml-auto mr-auto mt-8 text-black">
             Get Started
